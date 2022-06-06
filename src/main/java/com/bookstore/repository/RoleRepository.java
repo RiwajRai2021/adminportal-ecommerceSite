fd9df73f6ac7dcbf4a53b21d4c636847aba32a64
Riwaj Rai <riwajrai83@gmail.com>
2022-06-05 23:49:28 -0400

    Java_MVC_Ecommerce_website_BookStore_onTheMaking

---
+++ src/main/java/com/bookstore/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.bookstore.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bookstore.security.Role;
+
+public interface RoleRepository extends CrudRepository<Role,Long> {
+
+	Role findByName(String name);
+}
